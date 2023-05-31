b1aea3ee3b2e0fbabb02b68b44a48ae8ca089c85
Felippe <felipperian365@gmail.com>
2023-05-31 13:25:52 -0300

    Adicionado lib do swagger e alteração do model de turma

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,15 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     "DATE_INPUT_FORMATS": ["%d/%m/%Y"],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
 
 INSTALLED_APPS = [
@@ -40,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'teacher',
     'course',
     'student',
