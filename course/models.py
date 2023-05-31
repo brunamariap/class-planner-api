a2ae9611e259427d3d372ce20b0c30fc0ce1bab2
Bruna Maria <bruna.mariap@outlook.com>
2023-05-31 14:15:21 -0300

    Correção de fields dos models class e student

--- course/models.py
+++ course/models.py
@@ -46,7 +46,7 @@ class Class(models.Model):
         VESPERTINO = 'Tarde'
         NOTURNO = 'Noite'
     
-    class_leader = models.CharField(max_length=20,unique=True)
+    class_leader_id = models.CharField(max_length=20, unique=True, null=True, blank=True, default=None)
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE, db_column='course_id')
     reference_period = models.IntegerField()
     shift = models.CharField(max_length=10, choices=Shift.choices)
