9ddab974a7bbc27754efd635d1d6f0ad0b89d0fb
Felippe <felipperian365@gmail.com>
2023-06-07 13:24:50 -0300

    Listagem das disciplinas do estudante'

--- student/api/viewsets.py
+++ student/api/viewsets.py
@@ -8,7 +8,7 @@ from ..models import Student, StudentAlert
 from .serializers import StudentSerializer, StudentAlertSerializer
 
 from course.models import Discipline, Schedule, Class, TemporaryClass, ClassCanceled
-from course.api.serializers import ScheduleSerializer
+from course.api.serializers import ScheduleSerializer,DisciplineSerializer
 
 from datetime import datetime, timedelta, date
 
@@ -18,17 +18,38 @@ class StudentViewSet(ModelViewSet):
     
     @action(methods=['GET'], detail=False, url_path='(?P<student_id>[^/.]+)/schedules/week')
     def get_week_schedules(self, request, student_id):
-        student = Student.objects.get(id=student_id)
-        
-        disciplines = Discipline.objects.filter(id__in=student.disciplines.values_list('id', flat=True))
-        classes = Class.objects.filter(course_id=student.class_id.course_id)
-        schedules = Schedule.objects.filter(class_id__in=classes.values_list('id', flat=True), discipline_id__in=disciplines.values_list("id", flat=True))
-        
-        serializer = ScheduleSerializer(schedules, many=True, context={'request': request})
-        
-        return Response(
-            serializer.data, status=status.HTTP_200_OK
-        )
+        try:
+            student = Student.objects.get(id=student_id)
+            
+            disciplines = Discipline.objects.filter(id__in=student.disciplines.values_list('id', flat=True))
+            classes = Class.objects.filter(course_id=student.class_id.course_id)
+            schedules = Schedule.objects.filter(class_id__in=classes.values_list('id', flat=True), discipline_id__in=disciplines.values_list("id", flat=True))
+            
+            serializer = ScheduleSerializer(schedules, many=True, context={'request': request})
+            
+            return Response(
+                serializer.data, status=status.HTTP_200_OK
+            )
+        except:
+            return Response(
+                {'details': 'Não encontrado'}, status=status.HTTP_400_BAD_REQUEST
+            )
+    
+    @action(methods=['GET'], detail=False, url_path='(?P<student_id>[^/.]+)/disciplines')
+    def get_disciplines(self, request, student_id):
+        try:
+            student = Student.objects.get(id=student_id)
+            disciplines = Discipline.objects.filter(id__in=student.disciplines.values_list('id', flat=True))
+            
+            serializer = DisciplineSerializer(disciplines, many=True, context={'request': request})
+            
+            return Response(
+                serializer.data, status=status.HTTP_200_OK
+            )
+        except:
+            return Response(
+                {'details': 'Não encontrado'}, status=status.HTTP_400_BAD_REQUEST
+            )
 
 class StudentAlertViewSet(ModelViewSet):
     queryset = StudentAlert.objects.all()
